d895e94ab30451760ce0c57540733a79273f3455
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-10-26 18:31:34 +0300

    26.10

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhaba Dünya")
+
+let dolarBugun = 9.30
+
+ let dolarDun = 9.20
+ {
+     let dolarDun=9.10
+ }
+
+console.log(dolarDun)
+
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak Konut Kradisi","Kamu Konut Kradisi","Özel Kamu Konut Kradisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>") 
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
+
